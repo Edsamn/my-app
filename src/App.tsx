a4fd776a55697fea1887996cd8ea0a529da352d8
Edsamn <eduardomsribas@gmail.com>
2025-09-16 11:33:18 -0300

    cadastro de clientes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Greeting from "./components/Greeting.tsx";
 import Counter from "./components/Counter.tsx";
 import CustomerList from "./components/CustomerList.tsx";
+import CustomerForm from "./components/CustomerForm.tsx";
 
 function AppTSX() {
   return (
@@ -8,9 +9,11 @@ function AppTSX() {
       <h2>Primeiro Exercício</h2>
       <Greeting name="Eduardo" age={33}></Greeting>
       <Greeting name="Ana" age={25}></Greeting>
+      <Counter />
       <h2>Clientes</h2>
       <CustomerList />
-      <Counter />
+      <h2>Cadastro de Clientes</h2>
+      <CustomerForm />
     </div>
   );
 }
